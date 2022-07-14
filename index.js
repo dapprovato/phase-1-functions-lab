d5e59b2282085b2b4883ce0f6f5e905ee7907a12
Daria Approvato <dariaapprovato@gmail.com>
2022-07-14 18:18:29 -0400

    commit tree f63c5c8dc2db797913ac2e3f1e5bd8309ad0d949

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const hq = 42    
+        if (blocks > 42){ 
+            return blocks - hq
+        } else if (blocks < 42){
+            return hq - blocks
+        }
+}
+
+function distanceFromHqInFeet(feet) {
+    const hq = 42
+    const oneBlock = 264
+        if (feet > 42){
+            return (feet - hq) * oneBlock
+        } else if (feet < 42){
+            return (hq - feet) * oneBlock
+        }
+        
+}
+
+function distanceTravelledInFeet(travel1 , travel2) {
+    const oneBlock = 264
+    const feetTravelled = travel2 - travel1
+        if (feetTravelled > 0){
+            return feetTravelled * oneBlock
+        } else if (feetTravelled < 0){
+            return feetTravelled * (oneBlock * -1)
+        }
+}
+
+function calculatesFarePrice(start, destination) {
+    const oneBlock = 264
+    const feetTravelled = Math.abs(destination - start)
+        if (feetTravelled * oneBlock < 399){
+            return 0
+        } else if (feetTravelled * oneBlock <= 2001){
+            return ((feetTravelled * oneBlock) - 400) * 0.02
+        } else if (feetTravelled * oneBlock <= 2499){
+            return 25
+        } else if (feetTravelled * oneBlock > 2500){
+            return 'cannot travel that far'
+        }
+    }
